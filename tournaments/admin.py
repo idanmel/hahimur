162c89bdce8d2cf6965bceb66b2c48fd667b5b2f
idanmel <idanmel@gmail.com>
2024-11-04 14:37:53 +0200

    added predictions

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Match, Team, Tournament
+from .models import Match, Prediction, Team, Tournament
 
 admin.site.register(Tournament)
 admin.site.register(Team)
 admin.site.register(Match)
+admin.site.register(Prediction)
--- tournaments/models.py
+++ tournaments/models.py
@@ -1,10 +1,13 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.contrib.auth.models import User
 
 # matches
 # results
 # users
 # user scores
 # teams
+# prediction
 
 class Tournament(models.Model):
     name = models.CharField(max_length=200)
@@ -25,3 +28,10 @@ class Match(models.Model):
 
     def __str__(self):
         return f'{self.home_team} - {self.away_team} at {self.start_time}'
+
+
+class Prediction(models.Model):
+    friend = models.ForeignKey(User, on_delete=models.CASCADE)
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    home_score = models.IntegerField()
+    away_score = models.IntegerField()
