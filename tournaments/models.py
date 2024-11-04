a2d359e91d39e701658ebe6bbe413ce933b25fca
idanmel <idanmel@gmail.com>
2024-11-04 14:07:33 +0200

    created Teams

--- tournaments/models.py
+++ tournaments/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 
-# Create your models here.
-
-class Tournament(models.Model):
-    name = models.CharField(max_length=200)
-
-
-# tournaments
 # matches
 # results
 # users
 # user scores
 # teams
+
+class Tournament(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Team(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.name}'
