801c9f12e51408848778dc400833b40b9fb4bbf1
idanmel <idanmel@gmail.com>
2024-11-17 11:23:02 +0200

    Fix timezone warnings in tournament tests by using timezone-aware datetime objects

--- tournaments/tests.py
+++ tournaments/tests.py
@@ -1,6 +1,7 @@
 import datetime
-
 from django.test import TestCase
+from django.utils import timezone
+from zoneinfo import ZoneInfo
 
 from .models import Match, Stage, Team, Tournament, get_matches
 
@@ -12,19 +13,19 @@ class MatchTests(TestCase):
         team1 = Team.objects.create(name="Germany")
         team2 = Team.objects.create(name="Scotland")
         Match.objects.create(
-            start_time=datetime.datetime(2024, 6, 14, 19, 00),
+            start_time=datetime.datetime(2024, 6, 14, 19, 00, tzinfo=ZoneInfo("UTC")),
             stage=s,
             home_team=team1,
             away_team=team2
         )
         Match.objects.create(
-            start_time=datetime.datetime(2024, 6, 15, 19, 00),
+            start_time=datetime.datetime(2024, 6, 15, 19, 00, tzinfo=ZoneInfo("UTC")),
             stage=s,
             home_team=team1,
             away_team=team2
         )
         Match.objects.create(
-            start_time=datetime.datetime(2024, 6, 15, 19, 00),
+            start_time=datetime.datetime(2024, 6, 15, 19, 00, tzinfo=ZoneInfo("UTC")),
             stage=s,
             home_team=team1,
             away_team=team2
@@ -48,5 +49,3 @@ class MatchTests(TestCase):
         t = Tournament.objects.get(name="ASD")
         matches = get_matches(t.id, year=2024, month=6, day=15)
         self.assertEqual(len(matches), 2)
-
-
